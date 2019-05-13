06bd4d49ffaf6c53ae03f1853bc8887d56fedd35
Priya <50397267+priyadarshinitamije@users.noreply.github.com>
2019-05-13 18:32:44 +1200

    Update SalesController.cs

--- ReactOnboarding/Controllers/SalesController.cs
+++ ReactOnboarding/Controllers/SalesController.cs
@@ -26,8 +26,7 @@ namespace ReactOnboarding.Controllers
                 {
                     Id = x.Id,
                     DateSold = x.DateSold,
-                    CustomerName = x.Customer.Name,
-                    ProductName = x.Product.Name,
+                    
                     StoreName = x.Store.Name,
                 }).ToList();
                 var asdf = Json(salesList, JsonRequestBehavior.AllowGet);
