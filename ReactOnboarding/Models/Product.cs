cea520d0b7db45e8c8358581e0ee8a370055c358
Priya <50397267+priyadarshinitamije@users.noreply.github.com>
2019-05-13 18:35:15 +1200

    Update Product.cs

--- ReactOnboarding/Models/Product.cs
+++ ReactOnboarding/Models/Product.cs
@@ -22,7 +22,7 @@ namespace ReactOnboarding.Models
     
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ProductSold> ProductSolds { get; set; }
