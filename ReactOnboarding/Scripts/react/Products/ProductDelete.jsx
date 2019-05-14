0c29360dd1afa6340db759b293fd2c72e9214171
Priya <50397267+priyadarshinitamije@users.noreply.github.com>
2019-05-14 12:43:59 +1200

    Update ProductDelete.jsx

--- ReactOnboarding/Scripts/react/Products/ProductDelete.jsx
+++ ReactOnboarding/Scripts/react/Products/ProductDelete.jsx
@@ -8,5 +8,45 @@ export default class ProductDelete extends Component {
         this.state = {
 
         };
+ this.onDeleteSubmit = this.onDeleteSubmit.bind(this);
+        this.onClose = this.onClose.bind(this);
+    }
 
+    onDeleteSubmit(id) {
+        $.ajax({
+            url: "/Product/DeleteProduct",
+            type: "post",
+            data: { 'id': id }
+        });
+        window.location.reload()
+    }
+
+    onClose() {
+        this.setState({ showDeleteModal: false });
+        window.location.reload()
+    }
+
+    render() {
+
+        return (
+            <React.Fragment>
+                <Modal open={this.props.showDeleteModal} onClose={this.props.onClose} size='small'>
+                    <Modal.Header>Delete Product</Modal.Header>
+                    <Modal.Content>
+                        <h4>
+                            Are you sure?
+                        </h4>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button onClick={this.props.onClose} secondary >Cancel
+                            </Button>
+                        <Button onClick={() => this.onDeleteSubmit(this.props.delete)} className="ui red button">Delete
+                            <i className="x icon"></i>
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+            </React.Fragment>
+        )
+    }
+}
         
